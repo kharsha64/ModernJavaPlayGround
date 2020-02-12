17ac1932f21df1af1168041ef8b629364f85249f
kharsha <harsha.kr@gmail.com>
2020-02-12 22:39:00 +1100

    HowForEachWorks.java -V1

--- src/ForEachTest/HowForEachWorks.java
+++ src/ForEachTest/HowForEachWorks.java
@@ -1,4 +1,15 @@
 package ForEachTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class HowForEachWorks {
+
+    public static void main (String...a) {
+        List<Integer> numberList = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        Consumer<Integer> action = System.out::println;
+        numberList.stream().forEach(action);
+    }
 }
